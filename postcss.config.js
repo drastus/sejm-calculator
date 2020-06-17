1d13cf41ec7c5b203303411246e911ec240d9cb2
Michał Górny <zrchos@gmail.com>
2020-06-17 18:16:22 +0200

    Updates

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+"use strict";
+module.exports = {
+    plugins: {
+        'postcss-import': {},
+        'postcss-preset-env': {
+            browsers: 'last 2 versions',
+        },
+        'cssnano': {},
+    },
+};
+//# sourceMappingURL=postcss.config.js.map
