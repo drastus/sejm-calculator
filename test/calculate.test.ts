c7779ee384a87690465bc73b88e4665880a9a8c9
Michał Górny <zrchos@gmail.com>
2025-07-16 22:05:33 +0200

    Update tests

--- test/calculate.test.ts
+++ test/calculate.test.ts
@@ -1,22 +1,22 @@
 import calculateMandates from '../src/mandates';
 
-test('calculates correct results (1)', () => {
-	const support = [35.38, 30.7, 14.4, 8.61, 7.16];
+test('calculates correct results (last election)', () => {
+	const support = [35.38, 30.7, 14.4, 0, 8.61, 0, 7.16, 0];
 	const mandates = calculateMandates(support);
 
-	expect(mandates).toEqual([194, 157, 65, 26, 18, 0]);
+	expect(mandates).toEqual([194, 157, 65, 0, 26, 0, 18, 0, 0]);
 });
 
-test('calculates correct results (2)', () => {
-	const support = [30, 25, 20, 10, 6];
+test('calculates correct results (simulation 1)', () => {
+	const support = [25, 30, 5.5, 5.2, 6, 5.8, 10, 6];
 	const mandates = calculateMandates(support);
 
-	expect(mandates).toEqual([176, 130, 103, 39, 12, 0]);
+	expect(mandates).toEqual([158, 180, 16, 5, 19, 15, 48, 19, 0]);
 });
 
-test('calculates correct results (3)', () => {
-	const support = [39.7, 42.4, 0, 0, 7.2];
+test('calculates correct results (simulation 2)', () => {
+	const support = [39.7, 42.4, 0, 0, 0, 7.2, 0];
 	const mandates = calculateMandates(support);
 
-	expect(mandates).toEqual([223, 219, 0, 0, 18, 0]);
+	expect(mandates).toEqual([223, 216, 0, 0, 0, 21, 0, 0]);
 });
