123374e2e6fc18a30cdf0792fdaf94b825c0adf1
Michał Górny <zrchos@gmail.com>
2026-08-26 15:07:01 +0200

    Replace app with redirect to https://michalgorny.net/mandaty

--- src/pug.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=pug.js.map
--- test/matchMedia.mock.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-window.matchMedia = jest.fn().mockImplementation(function (query) { return ({
-    matches: false,
-    media: query,
-    onchange: null,
-    addListener: jest.fn(),
-    removeListener: jest.fn(),
-}); });
-//# sourceMappingURL=matchMedia.mock.js.map
